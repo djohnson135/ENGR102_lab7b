ac55b5a053c2ce2dffd15ce8ec8e1877bc1c4d78
djohnson135 <dathanljohnson135@gmail.com>
2019-02-28 08:01:12 -0600

    Test Git initial

---
+++ ENGR102_lab7b_Johnson_program1.py
@@ -0,0 +1,18 @@
+"""This program is meant to help give you practice with string manipulation.
+“Pig Latin” is a way of converting words in standard English to similar words that sound different. The
+rules for converting from standard words to Pig Latin are as follows:
+• If a word starts with a consonant, move the consonants to the end of the word, and add “ay” to
+the end.
+o e.g. “computer” becomes “omputercay”
+• If a word starts with a vowel, add “yay” on to the end of the word.
+o e.g. “engineering” becomes “engineeringyay”
+• Note: treat “y” as a vowel for this assignment.
+Write a program that repeatedly reads in a word from a user and converts it to Pig Latin. The program
+should continue reading words until the user enters “stop”.
+Challenge: Try to write a program where, instead of just one word, the user enters an entire sentence,
+and all words in the sentence are converted to Pig Latin."""
+
+
+
+
+print('yes')
