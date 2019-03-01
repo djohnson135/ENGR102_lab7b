4b584d09e9b99bb946463ac373f889a7ce4d86bd
djohnson135 <dathanljohnson135@gmail.com>
2019-02-28 18:53:06 -0600

    Completed without challenge activity

--- ENGR102_lab7b_Johnson_program1.py
+++ ENGR102_lab7b_Johnson_program1.py
@@ -11,8 +11,22 @@ Write a program that repeatedly reads in a word from a user and converts it to P
 should continue reading words until the user enters “stop”.
 Challenge: Try to write a program where, instead of just one word, the user enters an entire sentence,
 and all words in the sentence are converted to Pig Latin."""
+word = ''
+vowel = ['a', 'e', 'i', 'o','u','y','A','E','I','O','U','Y']
+stryay = 'yay'
+stray = 'ay'
+consanent = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Z', 'W']
+consanentlo = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'z', 'w']
+while word != 'stop':
+    word = input('what is your word?(stop quits code) ')
+    if word[0] in vowel:
+        word = word + stryay
+        print(word)
+    if word[0] in consanent or word[0] in consanentlo and word != 'stop':
+        wcons = word[0]
+        word = word[1:]
+        word = word + wcons + stray
+        print(word)
+    
 
-
-
-
-print('yes')
+    
