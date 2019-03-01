c7cd4d1a8da071954e085691e30ccb35f6f86b0c
djohnson135 <dathanljohnson135@gmail.com>
2019-02-28 21:06:01 -0600

    File instrunctions for program 2. Not sure how to do it

---
+++ ENGR102_lab7b_Johnson_program2.py
@@ -0,0 +1,24 @@
+"""This program is meant to give you practice with lists and looping on them, as well as practice with vector
+computations.
+Write a program that lets a user enter two vectors, A and B, of arbitrary dimension. You should allow
+the user to first enter the dimension of the vector, then get the elements of the two vectors from the
+user. Then, you should output (in a clearly labeled way), the results of these computations:
+• The magnitude of vector A and the magnitude of vector B
+• A + B
+• A – B
+• And the dot product (inner product) of A and B
+Note: You should use lists when solving this problem. Later we will see some other ways to work with
+vectors more directly."""
+
+
+"""
+By submitting this assignment, I agree to the following:
+“Aggies do not lie, cheat, or steal, or tolerate those who do”
+“I have not given or received any unauthorized aid on this assignment”
+
+Name : Dathan Johnson
+Section : 409
+Assignment : lab 7b - 2
+Date : 2-28-19
+"""
+
