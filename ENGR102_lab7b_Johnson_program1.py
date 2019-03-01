b3df7f0af84a69c18bd0f6f1e9e262e681737bf0
djohnson135 <dathanljohnson135@gmail.com>
2019-02-28 19:25:40 -0600

    Complete but ineffecient

--- ENGR102_lab7b_Johnson_program1.py
+++ ENGR102_lab7b_Johnson_program1.py
@@ -11,18 +11,48 @@ Write a program that repeatedly reads in a word from a user and converts it to P
 should continue reading words until the user enters “stop”.
 Challenge: Try to write a program where, instead of just one word, the user enters an entire sentence,
 and all words in the sentence are converted to Pig Latin."""
+
+"""
+By submitting this assignment, I agree to the following:
+“Aggies do not lie, cheat, or steal, or tolerate those who do”
+“I have not given or received any unauthorized aid on this assignment”
+
+
+Name : Dathan Johnson
+Section : 409
+Assignment : lab 7b - 1
+Date : 2-28-19
+"""
 word = ''
 vowel = ['a', 'e', 'i', 'o','u','y','A','E','I','O','U','Y']
 stryay = 'yay'
 stray = 'ay'
+space = ' '
+complete = ''
+sentence = []
 consanent = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Z', 'W']
 consanentlo = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'z', 'w']
 while word != 'stop':
     word = input('what is your word?(stop quits code) ')
-    if word[0] in vowel:
+    if space in word:
+        line = word.split()
+        for word in line:
+            if word[0] in vowel:
+                word = word + stryay
+                sentence.append(word)
+            elif word[0] in consanent or word[0] in consanentlo and word != 'stop':
+                wcons = word[0]
+                word = word[1:]
+                word = word + wcons + stray
+                sentence.append(word)
+        complete = ' '.join(i for i in sentence)
+        print(complete)
+        sentence = []
+    
+    elif word[0] in vowel:
         word = word + stryay
         print(word)
-    if word[0] in consanent or word[0] in consanentlo and word != 'stop':
+    elif word[0] in consanent or word[0] in consanentlo and word != 'stop':
         wcons = word[0]
         word = word[1:]
         word = word + wcons + stray
