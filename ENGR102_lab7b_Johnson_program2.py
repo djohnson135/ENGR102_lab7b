77ce28459ec58b2cdaa993f9d52dcacccbd2cae7
djohnson135 <dathanljohnson135@gmail.com>
2019-03-03 00:50:23 -0600

    complete but im hungry and tired so whatever

--- ENGR102_lab7b_Johnson_program2.py
+++ ENGR102_lab7b_Johnson_program2.py
@@ -1,15 +1,3 @@
-"""This program is meant to give you practice with lists and looping on them, as well as practice with vector
-computations.
-Write a program that lets a user enter two vectors, A and B, of arbitrary dimension. You should allow
-the user to first enter the dimension of the vector, then get the elements of the two vectors from the
-user. Then, you should output (in a clearly labeled way), the results of these computations:
-• The magnitude of vector A and the magnitude of vector B
-• A + B
-• A – B
-• And the dot product (inner product) of A and B
-Note: You should use lists when solving this problem. Later we will see some other ways to work with
-vectors more directly."""
-
 
 """
 By submitting this assignment, I agree to the following:
@@ -21,4 +9,41 @@ Section : 409
 Assignment : lab 7b - 2
 Date : 2-28-19
 """
+#define variables
+import math
+sumA = 0
+AB_add = 0
+#empty lists
+addlist = []
+AB_subtract = 0
+sublist = []
+sumB = 0
+dotproduct = 0
+vectorA = []
+vectorB = []
+#input for dimensin
+dimension = int(input('dimension: '))
+#range of the dimension to find vector values
+#assign vector to list
+for i in range(dimension):
+    vA = float(input('Vector A value: '))
+    vB = float(input('Vector B value: '))
+    vectorA.append(vA)
+    vectorB.append(vB)
+    AB_add = vectorA[i] + vectorB[i] #add vectors
+    addlist.append(AB_add)
+    AB_subtract = vectorA[i] - vectorB[i] #subtract vectors
+    sublist.append(AB_subtract)
+    dotproduct += vectorA[i] * vectorB[i] #find dotproduct
+    sumA += vectorA[i] ** 2
+    magA = abs(math.sqrt(sumA)) #find magnitude of vectors
+    sumB += vectorB[i] ** 2
+    magB = abs(math.sqrt(sumB))
+#print statements
+print('Magnitude of A: ',magA)
+print('Magnitude of B: ',magB)
+print('A + B: ', addlist)
+print('A - B: ', sublist)
+print('Dot product of A and B: ', dotproduct)
+
 
